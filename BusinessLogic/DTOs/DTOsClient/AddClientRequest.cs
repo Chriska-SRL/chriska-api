a467b523f9c438a53d6fe1a487c1245631e6e27b
Mathias Tabarez <tabarezmat@gmail.com>
2025-08-03 13:30:25 -0300

    Clients
    Clients

--- BusinessLogic/DTOs/DTOsClient/AddClientRequest.cs
+++ BusinessLogic/DTOs/DTOsClient/AddClientRequest.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLogic.Común.Audits;
 using BusinessLogic.DTOs.DTOsAudit;
+using BusinessLogic.DTOs.DTOsBankAccount;
 
 namespace BusinessLogic.DTOs.DTOsClient
 {
@@ -7,15 +8,15 @@ namespace BusinessLogic.DTOs.DTOsClient
     {
         public string Name { get; set; }
         public string RazonSocial { get; set; }
-        public string RUT { get; set; }
+        public string? RUT { get; set; }
         public string Address { get; set; }
         public string MapsAddress { get; set; }
         public string Phone { get; set; }
-        public string Email { get; set; }
+        public string? Email { get; set; }
         public string ContactName { get; set; }
         public string Schedule { get; set; }
-        public string Observations { get; set; }
+        public string? Observations { get; set; }
         public int ZoneId { get; set; }
-        public string Qualification { get; set; }
+        public List<BankAccountAddRequest> BankAccounts { get; set; }
     }
 }
--- BusinessLogic/DTOs/DTOsClient/UpdateClientRequest.cs
+++ BusinessLogic/DTOs/DTOsClient/UpdateClientRequest.cs
@@ -1,23 +1,24 @@
-﻿using BusinessLogic.Común.Audits;
-using BusinessLogic.DTOs.DTOsAudit;
+﻿using BusinessLogic.DTOs.DTOsAudit;
+using BusinessLogic.DTOs.DTOsBankAccount;
 
 namespace BusinessLogic.DTOs.DTOsClient
 {
     public class UpdateClientRequest : AuditableRequest
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string RazonSocial { get; set; }
-        public string RUT { get; set; }
-        public string Address { get; set; }
-        public string MapsAddress { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
-        public string ContactName { get; set; }
-        public string Schedule { get; set; }
-        public int LoanedCrates { get; set; }
-        public string Observations { get; set; }
-        public int ZoneId { get; set; }
-        public string Qualification { get; set; } 
+        public string? Name { get; set; }
+        public string? RazonSocial { get; set; }
+        public string? RUT { get; set; }
+        public string? Address { get; set; }
+        public string? MapsAddress { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? ContactName { get; set; }
+        public string? Schedule { get; set; }
+        public int? LoanedCrates { get; set; }
+        public string? Observations { get; set; }
+        public int? ZoneId { get; set; }
+        public List<BankAccountAddRequest>? BankAccounts { get; set; }
+        public string? Qualification { get; set; } 
     }
 }
