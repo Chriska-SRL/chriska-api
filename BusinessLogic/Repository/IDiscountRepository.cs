bd0e417335a0f5a3bb8f50ccdb159d62120926bf
Mathias Tabarez <tabarezmat@gmail.com>
2025-08-19 18:21:36 -0300

    GetBestDiscountAsync(int productId, int clientId)

--- BusinessLogic/Repository/IDiscountRepository.cs
+++ BusinessLogic/Repository/IDiscountRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace BusinessLogic.Repository
+﻿using BusinessLogic.Domain;
+
+namespace BusinessLogic.Repository
 {
-    public interface IDiscountRepository: IRepository<Domain.Discount>
+    public interface IDiscountRepository : IRepository<Domain.Discount>
     {
+        Task<Discount?> GetBestByProductAndClientAsync(Product product, Client client);
     }
 }
