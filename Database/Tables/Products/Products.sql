b6a67d7901883828dea8c898aa39606fb3c7eefc
Mathias Tabarez <tabarezmat@gmail.com>
2025-08-21 19:30:36 -0300

    fix barcode permite null

--- Database/Tables/Products/Products.sql
+++ Database/Tables/Products/Products.sql
@@ -6,7 +6,7 @@
     -- Campos de la entidad
     [Name] NVARCHAR(50) NOT NULL,
     [InternalCode] NCHAR(10) NOT NULL,
-    [BarCode] NCHAR(13) NOT NULL,
+    [BarCode] NCHAR(13) NULL,
     [UnitType] NCHAR(10) NOT NULL,
     [Price] MONEY NOT NULL, 
     [Description] NVARCHAR(255) NOT NULL, 
