aed35fae1141bba349421ee97ff5242f8a61c1dc
mathiastabarez <tabarezmat@gmail.com>
2025-09-07 10:57:29 -0300

    Fix: Particiones En Productitems.Quantity

--- BusinessLogic/DTOs/DTOsProductItem/ProductItemResponse.cs
+++ BusinessLogic/DTOs/DTOsProductItem/ProductItemResponse.cs
@@ -6,7 +6,7 @@ namespace BusinessLogic.DTOs.DTOsProductItem
     public class ProductItemResponse
     {
         public decimal Quantity { get; set; }
-        public decimal? Weight { get; set; }
+        public int? Weight { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal Discount { get; set; }
         public ProductResponse? Product { get; set; }
